0750fbbb2828581a79a84903933fdc941d41e175
jw526 <jw526@njit.edu>
2022-11-21 12:33:34 -0400

    added client info

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,14 +1,27 @@
-const { json } = require("body-parser");
 
 function updateCustomerData(){
     //fetch to our GET
-    fetch(url)
-    .then(res=>json())
+    fetch('http://localhost:3333/customer')
+    .then(res=>res.json())
     .then(json=>{
         //do something
+        const customerData = document.querySelector('#customerList');
+        for(const cust of json){
+            customerData.innerHTML += `
+            <b>${cust.FirstName}</b>
+            <b>${cust.LastName}</b>
+            <br>
+            `;
+            
+            // JSON.stringify(cust.FirstName);
+        }
+
+        // customerData.innerText = JSON.stringify(json);
     });
-    const customerData = document.querySelector('#customerList');
-    customerData.innerText = "blah";
 }
 
 updateCustomerData();
+
+function addNewCustomer(){
+
+}
