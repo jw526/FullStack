0699eca0cfeb3d214cef60dc670b513fd59ccc41
jw526 <jw526@njit.edu>
2022-12-04 17:55:55 -0400

    added update
    -added editable table and update customer

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -5,23 +5,88 @@ function updateCustomerData(){
     .then(res=>res.json())
     .then(json=>{
         //do something
-        const customerData = document.querySelector('#customerList');
+        const divTag = document.querySelector('div#editableTable');
+        let html = '<table class="striped"><thead><th>First Name</th><th>LastName</th><th>Email</th><th>Double Click to edit</th></thead><tbody>';
         for(const cust of json){
-            customerData.innerHTML += `
-            <b>${cust.FirstName}</b>
-            <b>${cust.LastName}</b>
-            <br>
+            html += `
+            <tr>
+                <td class="hidden_id">${cust.CustomerId}</td>
+                <td>${cust.FirstName}</td>
+                <td>${cust.LastName}</td>
+                <td>${cust.Email}</td>
+                <td><button class="update">Update</button><button class="delete">Delete</button></td>
+            </tr>
             `;
             
             // JSON.stringify(cust.FirstName);
         }
-
+        divTag.innerHTML = html + '</tbody></table>';
+        addEventListeners();
         // customerData.innerText = JSON.stringify(json);
     });
 }
-
+//display customer data on the webpage
 updateCustomerData();
 
-function addNewCustomer(){
+document.querySelector('#add').addEventListener('click', addCustomer);
+//using form submit instead of button
+// document.querySelector('form').addEventListener('submit', addCustomer);
+function addEventListeners()
+{
+    const tdTags = document.querySelectorAll('td');
+    const updateBtns = document.querySelectorAll('.update');
     
+    for(const tdTag of tdTags){
+    
+        tdTag.addEventListener('dblclick', editInfo);
+    }
+    for(const updateBtn of updateBtns){
+    
+        updateBtn.addEventListener('click', updateCustomer);
+    }
+}
+
+
+function addCustomer(event)
+{
+    event.preventDefault();
+    
+    //grab the info from the form
+    const fd = new FormData(document.querySelector('form'));
+    
+    //post the info to our server
+    fetch('http://localhost:3333/customer',{method:'post', body:fd});
+    
+    updateCustomerData();
+
+}
+function editInfo(event)
+{
+    // event.preventDefault();
+    event.target.setAttribute('contenteditable',true);
+    //grad the info from the form
+    // alert('hey');
+}
+function updateCustomer(event)
+{
+    
+    const allTds = event.target.parentElement.parentElement.children;
+    let updateJson = {};
+    const custID = allTds[0].textContent;
+
+    updateJson['FirstName'] = allTds[1].textContent;
+    updateJson['LastName'] = allTds[2].textContent;
+    updateJson['Email'] = allTds[3].textContent;
+    
+    updateJson = JSON.stringify(updateJson);
+    console.dir(updateJson);
+    fetch('http://localhost:3333/customer/'+custID, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method:'put', 
+        body:updateJson
+    });
+    updateCustomerData();
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const database = require('better-sqlite3');
+const cors = require('cors');
+const { json } = require('body-parser');
 
 const port = 3333;
 const app = express();
 const upload = multer();
+app.use(cors({origin:'*'}));
+app.use(express.json());
 
 const db = new database('./database/Chinook_Sqlite.sqlite');
 
 app.post('/customer',upload.none(),(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
     const sql = "INSERT INTO Customer(FirstName, LastName, Email) VALUES(?,?,?)";
     const statement = db.prepare(sql);
     const result = statement.run([req.body.FirstName, req.body.LastName, req.body.Email]);
@@ -18,10 +22,17 @@ app.post('/customer',upload.none(),(req, res)=>{
 
 app.get('/customer',(req,res)=>{
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
     const sql = "SELECT * FROM Customer";
     const statement = db.prepare(sql);
     const result = statement.all();
+    res.json(result);
+});
+
+app.put('/customer/:id', (req, res) => {
+
+    const sql = "UPDATE Customer SET FirstName=?, LastName=?,Email=? WHERE CustomerId=?";
+    const statement = db.prepare(sql);
+    const result = statement.run([req.body.FirstName, req.body.LastName, req.body.Email, req.params.id]);
     res.json(result);
 });
 
